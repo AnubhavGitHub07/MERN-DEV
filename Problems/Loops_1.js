94e2757166851377dd9a5498b8f68dbdf1c6fc89
AnubhavGitHub07 <prince1dwivedi00@gmail.com>
2025-11-24 00:16:13 +0530

    Problem 1

--- Problems/Loops_1.js
+++ Problems/Loops_1.js
@@ -1,1 +1,6 @@
-// Write a program to print the multiplication table of a given number using a loop.t 
+// Write a program to print the multiplication table of a given number using a loop.
+
+ let x = 5;
+ for(let i = 1 ; i<=10 ; i++ ){
+    console.log(x*i);
+ }
